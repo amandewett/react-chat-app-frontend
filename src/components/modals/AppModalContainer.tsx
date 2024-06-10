d6e37d331e9195367fa86becfb3f575e6131a1dc
Aman Dewett <amandewett@gmail.com>
2024-06-10 10:21:15 -0400

    updates
    - fixed index issue with modal
    - updated agit actions file

--- src/components/modals/AppModalContainer.tsx
+++ src/components/modals/AppModalContainer.tsx
@@ -4,7 +4,7 @@ import { AppModalContainerProps } from "../../utils/customTypes";
 const AppModalContainer = ({ isOpen, onClose, children, closeOnOverlayClick = true }: AppModalContainerProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered closeOnOverlayClick={closeOnOverlayClick}>
-      <ModalOverlay zIndex={10}>
+      <ModalOverlay zIndex={0}>
         <ModalContent m={[4, 3, 0]} bgColor="#262626">
           {children}
         </ModalContent>
